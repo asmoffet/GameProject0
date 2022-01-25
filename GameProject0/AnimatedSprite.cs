3e5e8d20344da73e9d11c321f984319161f3a5ef
asmoffet <54548893+asmoffet@users.noreply.github.com>
2022-01-25 14:08:47 -0600

    Add project files.

---
+++ GameProject0/AnimatedSprite.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace GameProject0
+{
+    
+    public class AnimatedSprite
+    {
+        private double animationTimer;
+
+        private short animationFrame = 1;
+        private short animationDirection = 1;
+
+        public void Draw(GameTime gametime, SpriteBatch spriteBatch, Texture2D texture, Vector2 position)
+        {
+            //update time
+            animationTimer += gametime.ElapsedGameTime.TotalSeconds;
+
+            //update frame
+            if (animationTimer > .3)
+            {
+                
+                if (animationFrame >= 3) animationDirection = -1;
+                if (animationFrame <= 1) animationDirection = 1;
+
+                animationFrame += animationDirection;
+                
+                animationTimer -= .3;
+            }
+            //draw the sprite
+            var source = new Rectangle(animationFrame * 16 + 320, 80, 16, 16);
+            spriteBatch.Draw(texture, position, source, Color.White, 0f, new Vector2(0,0), (float)5, SpriteEffects.None,0);
+        }
+    }
+}
---
+++ GameProject0/Game1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace GameProject0
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private AnimatedSprite[] _animatedSprites;
+        private Texture2D _spriteSheet;
+        private SpriteFont _bangers;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _animatedSprites = new AnimatedSprite[]
+            {
+                new AnimatedSprite(),
+                new AnimatedSprite()
+            };
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _spriteSheet = Content.Load<Texture2D>("colored_packed");
+            _bangers = Content.Load<SpriteFont>("bangers");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _animatedSprites[0].Draw(gameTime, _spriteBatch, _spriteSheet, new Vector2(25, 400));
+            _animatedSprites[1].Draw(gameTime, _spriteBatch, _spriteSheet, new Vector2(700, 400));
+            _spriteBatch.Draw(_spriteSheet, new Vector2(0,0), new Rectangle(96, 16, 16, 16), Color.White, 0f, new Vector2(0, 0), (float)5, SpriteEffects.None, 0);
+            _spriteBatch.Draw(_spriteSheet, new Vector2(GraphicsDevice.Viewport.Width - 80, 0), new Rectangle(96, 16, 16, 16), Color.White, 0f, new Vector2(0, 0), (float)5, SpriteEffects.None, 0);
+            _spriteBatch.DrawString(_bangers, "Springy Cactus", new Vector2(GraphicsDevice.Viewport.Width / 2 - 145, GraphicsDevice.Viewport.Height / 2), Color.Goldenrod);
+            _spriteBatch.DrawString(_bangers, "Press esc or B to exit.", new Vector2(GraphicsDevice.Viewport.Width / 2 - 185, GraphicsDevice.Viewport.Height - 45), Color.Black);
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
